1671655e6e4c96426a4f22a939aae7ff48d66339
adityachandrafb <adityachandrafb@gmail.com>
2021-11-27 09:51:59 +0700

    ulang commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export {default as Navbar} from './Navbar/Navbar';
-export {default as Products} from './Products/Products';
+// export {default as Products} from './Products/Products';
 export {default as Footer} from './Footer/Footer';
 export {default as DropdownIphone} from './DropdownIphone/DropdownIphone';
 export {default as DropdownIpad} from './DropdownIpad/DropdownIpad';
